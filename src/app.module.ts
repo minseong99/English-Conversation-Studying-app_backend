0046f49a997db7eb99df234bb56f825a296379db
minseong99 <162446292+minseong99@users.noreply.github.com>
2025-04-30 15:20:01 +0900

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
-import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import {  ThrottlerGuard } from '@nestjs/throttler';
 import { CacheModule } from '@nestjs/cache-manager';
 import { APP_GUARD } from '@nestjs/core';
 import { redisStore } from 'cache-manager-redis-store';
@@ -12,26 +12,18 @@ import { WordChainModule } from './wordchain/wordchain.module';
 import { GameModule } from './game/game.module';
 import { HealthModule } from './health/health.module';
 
+
+
 @Module({
   imports: [
-    // 1) ThrottlerModule를 Upstash Redis 기반으로 설정
-    ThrottlerModule.forRootAsync({
-      useFactory: () => ({
-        ttl: 60,            // 1분 동안
-        limit: 30,          // 최대 30회 요청
-        store: redisStore({
-          url: process.env.REDIS_URL,         // ex) rediss://:token@upstash... 
-          // token 은 REDIS_TOKEN 으로 자동 읽힙니다.
-        }),
-      }),
-    }),
+  
 
     // 2) CacheModule 역시 Upstash Redis REST 로 통일
     CacheModule.registerAsync({
       isGlobal: true,
-      useFactory: () => ({
-        store: redisStore({
-          url: process.env.REDIS_URL,
+      useFactory: async() => ({
+        store: await redisStore({
+          url: process.env.REDIS_URL!,
         }),
         ttl: 60 * 5,       // 5분 캐시
       }),
